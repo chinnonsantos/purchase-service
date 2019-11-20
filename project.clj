1d6ba4a95bf0ea28bf56f37bf5f30768979f0677
Chinnon Santos <chinnonsantos@gmail.com>
2019-11-20 09:52:34 +0000

    Fixing embedded nREPL

--- project.clj
+++ project.clj
@@ -18,9 +18,11 @@
                                   [midje "1.9.9"] ;; TDD
                                   [ring/ring-core "1.7.1"]
                                   [ring/ring-jetty-adapter "1.7.1"] ;; Ring server abstraction
+                                  [cljfmt "0.6.5"] ;; Code formatting
                                   ]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-cloverage "1.1.2"] ;; Test coverage
+                             [cider/cider-nrepl "0.22.4"] ;; embedded nREPL
                              ]}}
   :test-paths ["test/unit"
                "test/assertion"])
